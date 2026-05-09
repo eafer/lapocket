eafer/lapocket

commit 073aa8996516c82c3ca6d91e7fee96a36b5c6617

Implement the display registers that report width
I'm finally getting some output on the display, but it's only a single
line.

The function in charge here is <0x800370B0>. It takes four arguments,
three of them being the image to output, its width and its height. After
using the other argument for some mysterious display register
configuration, two bytes are read from 0xb4000016 and 0xb4000017 and
reassembled into a word. The result is zero, because all unimplemented
registers return that value.

Then there are two nested loops to copy each column and row of the
source image into the framebuffer. The interesting thing is that, after
each of the inner loops is done copying a whole row, the difference
between the 0xb4000016 word and the row's width gets added to the
framebuffer offset.

The obvious conclusion is that this word is the framebuffer width, and
the function is supposed to accept images that are smaller than the
screen and just print them to the upper-left corner. But if the display
reports a zero width, the output offset will get reset after each inner
loop, and the rows will overwrite each other, explaining the single line
of output.

Implement the 0xb4000016 and 0xb4000017 registers correctly. With that
change, the splash hummingbird is now displayed in all its glory. Though
only in black and white for now, since I'm yet to figure out the color
palette. Rig a simple test to celebrate.

lapocket.c
@@ -763,7 +763,9 @@ uint8_t memory[MEMORY_SIZE] = {0};
  */
 #define DISPLAY_OFF			0x14000000
 #define	DISPLAY_FB_OFF		0x14200000
-#define DISPLAY_FB_SIZE		(240 * 320)
+#define DISPLAY_FB_WIDTH	240
+#define DISPLAY_FB_HEIGHT	320
+#define DISPLAY_FB_SIZE		(DISPLAY_FB_WIDTH * DISPLAY_FB_HEIGHT)
 #define DISPLAY_RAM_SIZE	0x00080000
 struct display {
 	/* Framebuffer and display ram. TODO: what is the ram for? Rename this? */
@@ -963,6 +965,41 @@ struct cpu {
 /* Extra state flags */
 #define EXTRA_IN_DELAYED	1U	/* Executing the instruction after a branch */
 #define EXTRA_POWER_DOWN	2U	/* In power-down mode */
+
+/*
+ * Lots of other display registers get accessed below the display ram. Most of
+ * them will probably never matter to the emulator. These are the few that I've
+ * figured out so far.
+ */
+#define DISPLAY_WIDHT_L_OFF		0x14000016	/* Screen width in words (low byte) */
+#define DISPLAY_WIDHT_H_OFF		0x14000017	/* Screen width in words (high byte) */
+
+static bool is_display_regs_byte_address(uint32_t addr)
+{
+	if ((addr & 0xFF000000) != DISPLAY_OFF)
+		return false;
+	if (addr >= DISPLAY_FB_OFF && addr < DISPLAY_FB_OFF + DISPLAY_RAM_SIZE)
+		return false;
+	return true;
+}
+
+static uint8_t display_read_byte_reg(uint32_t addr)
+{
+	switch (addr) {
+	case DISPLAY_WIDHT_L_OFF:
+		return (DISPLAY_FB_WIDTH >> 1) & 0x00FF;
+	case DISPLAY_WIDHT_H_OFF:
+		return ((DISPLAY_FB_HEIGHT >> 1) & 0xFF00) >> 8;
+	default:
+		if (addr >= DISPLAY_FB_OFF + DISPLAY_RAM_SIZE) {
+			panic("Unsupported display register 0x%.8x\n", addr);
+			return 0;
+		} else {
+			notice("Reading from unknown display register 0x%.8x (PC: 0x%.8x)\n", addr, cpu.PC);
+			return 0;
+		}
+	}
+}
 
 /*
  * The Card Information Structure (CIS) for a CompactFlash memory card is
@@ -4834,14 +4871,8 @@ static uint8_t read_byte(uint32_t addr)
 	case MEMORY_SHADOW:
 		return *(uint8_t *)(memory + (addr & MEMORY_MASK));
 	case DISPLAY_OFF:
-		if (addr >= DISPLAY_FB_OFF + DISPLAY_RAM_SIZE) {
-			panic("Unsupported display register 0x%.8x\n", addr);
-			return 0;
-		}
-		if (addr < DISPLAY_FB_OFF) {
-			notice("Reading from unknown display register 0x%.8x (PC: 0x%.8x)\n", addr, cpu.PC);
-			return 0;
-		}
+		if (is_display_regs_byte_address(addr))
+			return display_read_byte_reg(addr);
 		return display.fb[addr - DISPLAY_FB_OFF];
 	case 0x13000000:
 		return xB3A_read_byte_reg(addr);

tests/0033
@@ -1,0 +1,16 @@
+# Tests that the hummingbird is drawn to screen correctly
+
+# Patch away all writes to display registers that aren't implemented, otherwise
+# we'll get flooded with unknown register notices.
+patch 0x800370C6 0x0009
+patch 0x800370E2 0x0009
+patch 0x800370EA 0x0009
+patch 0x800370F2 0x0009
+
+set R15 0x0C001000 # Mock a stack somewhere
+set PC 0x80033DE2
+break 0x80033DEC
+run
+
+print CRC
+exit

tests/0033.out
@@ -1,0 +1,1 @@
+Display CRC: a30b5bb1

commit 88b665cd3d701d2bfc47747df8691a29d436de71

Avoid out-of-bounds access to display memory
I noticed this bug during the recent panic overhaul. Patches
34f1b3a3fa83 ("Implement the palette") and 073aa8996516 ("Implement the
display registers that report width") inadvertently removed the boundary
checks for both reads and writes to bytes in the display memory.

Ultimately the reason for this mess is that all memory access should
follow the same pattern: first call a function to check if the memory
address belongs to a given component, and if so call another function to
perform the actual operation. Old code deviates from this a lot though,
and in this case it caused a confusion. To fix the bug, adjust the code
to the pattern.

lapocket.c
@@ -1005,13 +1005,16 @@ static bool is_display_regs_byte_address(uint32_t addr)
 {
 	if ((addr & 0xFF000000) != DISPLAY_OFF)
 		return false;
-	if (addr >= DISPLAY_FB_OFF && addr < DISPLAY_FB_OFF + DISPLAY_RAM_SIZE)
-		return false;
 	return true;
 }
 
 static int display_read_byte_reg(uint32_t addr, uint8_t *val_p)
 {
+	if (addr >= DISPLAY_FB_OFF && addr < DISPLAY_FB_OFF + DISPLAY_RAM_SIZE) {
+		*val_p = display.fb[addr - DISPLAY_FB_OFF];
+		return 0;
+	}
+
 	switch (addr) {
 	case DISPLAY_WIDHT_L_OFF:
 		*val_p = (DISPLAY_FB_WIDTH >> 1) & 0x00FF;
@@ -1032,6 +1035,11 @@ static int display_read_byte_reg(uint32_t addr, uint8_t *val_p)
 
 static int display_write_byte_reg(uint32_t addr, uint8_t val)
 {
+	if (addr >= DISPLAY_FB_OFF && addr < DISPLAY_FB_OFF + DISPLAY_RAM_SIZE) {
+		display.fb[addr - DISPLAY_FB_OFF] = val;
+		return 0;
+	}
+
 	switch (addr) {
 	case DISPLAY_PAL_IDX_OFF:
 		display.pal_idx = val;
@@ -5021,8 +5029,7 @@ static int read_byte(uint32_t addr, uint8_t *val_p)
 	case DISPLAY_OFF:
 		if (is_display_regs_byte_address(addr))
 			return display_read_byte_reg(addr, val_p);
-		*val_p = display.fb[addr - DISPLAY_FB_OFF];
-		return 0;
+		break;
 	case 0x13000000:
 		return xB3A_read_byte_reg(addr, val_p);
 	case 0x18000000:
@@ -5228,8 +5235,7 @@ static int write_byte(uint32_t addr, uint8_t val)
 	case DISPLAY_OFF:
 		if (is_display_regs_byte_address(addr))
 			return display_write_byte_reg(addr, val);
-		display.fb[addr - DISPLAY_FB_OFF] = val;
-		return 0;
+		break;
 	case 0x13000000:
 		return xB3A_write_byte_reg(addr, val);
 	case 0x18000000: