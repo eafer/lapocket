0261987813476cd365fb2f6a0ae53913b9a2dda0
Ernesto A. Fernández <ernesto.mnd.fernandez@gmail.com>
2026-05-10 18:42:03 -0300

    Check for TLB entry issues in the right order
    Looking at the manual more carefully, there is a specific order that
    needs to be followed when triggering tlb exceptions. First check if a
    matching entry exists, then check if it's valid, then check if we have
    the right privilege, and finally check if it's dirty. At this point this
    doesn't make any difference, but do it correctly.

--- lapocket.c
+++ lapocket.c
@@ -4028,8 +4028,6 @@ static int mmu_virt_to_phys(uint32_t va, uint32_t *pa, bool write)
 				continue;
 			if (!(tlb_addr & TLB_V))
 				break;
-			if (write && (tlb_data & TLB_D))
-				return panic("Writing to non-dirty page\n");
 			protection = (tlb_data & TLB_PR_MASK) >> TLB_PR_SHIFT;
 			if (protection == 0x02) {
 				if (write)
@@ -4037,6 +4035,8 @@ static int mmu_virt_to_phys(uint32_t va, uint32_t *pa, bool write)
 			} else if (protection != 0x03) {
 				return panic("Unsupported page protection %d\n", protection);
 			}
+			if (write && (tlb_data & TLB_D))
+				return panic("Writing to non-dirty page\n");
 			*pa = mmu_tlb_to_pa(tlb_data) + (va - vpage_addr);
 			return 0;
 		}
