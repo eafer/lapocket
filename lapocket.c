eafer/lapocket

commit bec7d2bc73050d9fe1cf5c15c9bc085f0404a569

Avoid tight loops inside update_clocks()
One more attempt to meaningfully reduce the cost of update_clocks():
get rid of a few long-running while loops and replace them with
divisions and modulos. This patch should not alter functionality.

lapocket.c
@@ -8869,26 +8869,29 @@ static void increment_bsc_rfcr(void)
 }
 
 /* Increment the RTCNT bsc register */
-static void increment_bsc_rtcnt(void)
+static void increment_bsc_rtcnt(unsigned int inc)
 {
-	bsc.RTCNT += 1;
-	if (bsc.RTCNT == bsc.RTCOR) {
+	while ((uint16_t)(bsc.RTCOR - bsc.RTCNT) <= inc) {
+		inc -= (uint16_t)(bsc.RTCOR - bsc.RTCNT);
 		bsc.RTCNT = 0;
 		bsc.RTCSR |= RTCSR_CMF;
 		if (bsc.RTCSR & RTCSR_CMIE)
 			(void)panic("Interrupt from CMF not implemented\n");
 		increment_bsc_rfcr();
 	}
+	bsc.RTCNT += inc;
 }
 
 /* Decrement the TCNT0-2 TMU register */
-static void decrement_tmu_tcnt(int i)
+static void decrement_tmu_tcnt(int i, unsigned int dec)
 {
-	tmu.TCNT[i] -= 1;
-	if (tmu.TCNT[i] == 0xFFFFFFFFU) {
+	while (tmu.TCNT[i] < dec) {
+		dec -= tmu.TCNT[i];
+		--dec;
 		tmu.TCNT[i] = tmu.TCOR[i];
 		tmu.TCR[i] |= TCR_UNF;
 	}
+	tmu.TCNT[i] -= dec;
 }
 
 /* Get the prescale value for the TCNT0-2 clocks */
@@ -8948,6 +8951,7 @@ static long long last_clock_update = 0;
 static void update_clocks(void)
 {
 	long long cycle;
+	unsigned int cycle_count;
 	int i;
 
 	if (headless) {
@@ -8979,10 +8983,9 @@ static void update_clocks(void)
 		 * let's go with that for now. This CKS follows CKIO/16.
 		 */
 		cycle = 50 * 16;
-		while (nanosecs - bsc.pretime >= cycle) {
-			bsc.pretime += cycle;
-			increment_bsc_rtcnt();
-		}
+		cycle_count = (nanosecs - bsc.pretime) / cycle;
+		bsc.pretime += cycle_count * cycle;
+		increment_bsc_rtcnt(cycle_count);
 		break;
 	default:
 		(void)panic("RTCNT clock input 0x%x not implemented\n", bsc.RTCSR & RTCSR_CKS);
@@ -8995,20 +8998,18 @@ static void update_clocks(void)
 		if ((tmu.TCR[i] & TCR_TPSC) == 4) {
 			/* The real-time clock has a frequency of 32.768 kHz */
 			cycle = 30517LL * TMU_PRETIME_MULT;
-			while (TMU_PRETIME_MULT * nanosecs - tmu.pretime[i] >= cycle) {
-				tmu.pretime[i] += cycle;
-				decrement_tmu_tcnt(i);
-			}
+			cycle_count = (TMU_PRETIME_MULT * nanosecs - tmu.pretime[i]) / cycle;
+			tmu.pretime[i] += cycle_count * cycle;
+			decrement_tmu_tcnt(i, cycle_count);
 		} else if ((tmu.TCR[i] & TCR_TPSC) < 4) {
 			/*
 			 * The peripheral clock is set to ~22.12Mhz (or so it seems from
 			 * looking at <8003BB0C>).
 			 */
 			cycle = 45211243LL * tmu_prescaler(i);
-			while (TMU_PRETIME_MULT * nanosecs - tmu.pretime[i] >= cycle) {
-				tmu.pretime[i] += cycle;
-				decrement_tmu_tcnt(i);
-			}
+			cycle_count = (TMU_PRETIME_MULT * nanosecs - tmu.pretime[i]) / cycle;
+			tmu.pretime[i] += cycle_count * cycle;
+			decrement_tmu_tcnt(i, cycle_count);
 		} else {
 			(void)panic("TMU timer prescaler 0x%x not implemented\n", tmu.TCR[i] & TCR_TPSC);
 			return;

commit 7540704374641dcc20f8393ecef46ea2e98b1244

Avoid infinite loop when RTCOR and RTCNT are zero
Boot sometimes gets stuck very early, seemingly at random. The problem
was introduced by commit bec7d2bc7305 ("Avoid tight loops inside
update_clocks()"), which "should not alter functionality", but clearly
it did.

When RCOR and RTCNT are both zero, as is the case right after boot, the
loop inside increment_bsc_rtcnt() will never decrease its counter, so it
can't return. The function at <0x8002F968> starts the timer before
setting either register, so there is a small window of opportunity where
we'll become stuck if update_clocks() triggers.

The manual, as far as I can see, isn't clear about what should happen if
RCOR and RTCNT match *before* RTCNT gets updated. I think it's
reasonable to ignore the match and just keep increasing RTCNT until it
wraps around, so do that. I doubt the details will matter anyway.

lapocket.c
@@ -8937,8 +8937,22 @@ static void increment_bsc_rfcr(void)
 /* Increment the RTCNT bsc register */
 static void increment_bsc_rtcnt(unsigned int inc)
 {
-	while ((uint16_t)(bsc.RTCOR - bsc.RTCNT) <= inc) {
-		inc -= (uint16_t)(bsc.RTCOR - bsc.RTCNT);
+	unsigned int dist;
+
+	while (true) {
+		dist = bsc.RTCOR - bsc.RTCNT;
+
+		/*
+		 * RTCOR and RTCNT may have been initialized with the same value (they
+		 * are both zero on boot for example). I'm not entirely sure what
+		 * should happen here but this looks somewhat sensible.
+		 */
+		if (dist == 0)
+			dist = 1 << 16;
+
+		if (dist > inc)
+			break;
+		inc -= dist;
 		bsc.RTCNT = 0;
 		bsc.RTCSR |= RTCSR_CMF;
 		if (bsc.RTCSR & RTCSR_CMIE)